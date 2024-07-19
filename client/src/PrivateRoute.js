0bd22fc4f2aa96bdddbc5c05982e83ee8dbf510b
kaos <69476323+1tzkaos@users.noreply.github.com>
2024-07-19 05:55:33 -0500

    Update PrivateRoute.js

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
+const crypto = require("crypto");
+
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
     render={(props) =>
-      localStorage.getItem("user_token") ? (
+      crypto
+        .createHash("sha256")
+        .update(localStorage.getItem("token"))
+        .digest("hex") === localStorage.getItem("owner") ? (
         <Component {...props} />
       ) : (
         <Redirect
